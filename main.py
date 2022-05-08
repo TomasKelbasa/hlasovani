e5497154d74dd89ecda95143bbb00c56cfba2904
Tomáš Kelbasa <91247802+TomasKelbasa@users.noreply.github.com>
2022-05-08 15:04:39 +0200

    Aktualizovat main.ts, main.py

--- main.py
+++ main.py
@@ -1,20 +1,31 @@
 radio.set_group(15)
+canVote = True
+radio.set_transmit_serial_number(True)
 
 def on_forever():
-    if input.button_is_pressed(Button.A):
-        radio.send_value("answer", 65)
-    if input.button_is_pressed(Button.B):
-        radio.send_value("answer", 66)
-    if input.pin_is_pressed(TouchPin.P0):
-        radio.send_value("answer", 67)
-    if input.pin_is_pressed(TouchPin.P1):
-        radio.send_value("answer", 68)
-    if input.pin_is_pressed(TouchPin.P2):
-        radio.send_value("answer", 69)
+    if canVote:
+        if input.button_is_pressed(Button.A):
+            radio.send_value("answer", 0)
+        if input.button_is_pressed(Button.B):
+            radio.send_value("answer", 1)
+        if input.pin_is_pressed(TouchPin.P0):
+            radio.send_value("answer", 2)
+        if input.pin_is_pressed(TouchPin.P1):
+            radio.send_value("answer", 3)
+        if input.pin_is_pressed(TouchPin.P2):
+            radio.send_value("answer", 4)
 basic.forever(on_forever)
 
 #pokud je hlas serverem přijat, server pošle potvrzení a klient zobrazí znak YES
+#klient také přijímá informaci zda je možno hlasovat, pokud není možno hlasovat tak zobrazí křížek
 def on_received_value(name, value):
     if name == "confirm" and value == control.device_serial_number():
         basic.show_icon(IconNames.YES)
+    elif name == "voting":
+        if value == 0:
+            canVote = False
+            basic.show_icon(IconNames.NO, 1)
+        elif value == 1:
+            canVote = True
+            basic.clear_screen()
 radio.on_received_value(on_received_value)
--- main.ts
+++ main.ts
@@ -1,30 +1,46 @@
 radio.setGroup(15)
+let canVote = true
+radio.setTransmitSerialNumber(true)
 basic.forever(function on_forever() {
-    if (input.buttonIsPressed(Button.A)) {
-        radio.sendValue("answer", 65)
-    }
-    
-    if (input.buttonIsPressed(Button.B)) {
-        radio.sendValue("answer", 66)
-    }
-    
-    if (input.pinIsPressed(TouchPin.P0)) {
-        radio.sendValue("answer", 67)
-    }
-    
-    if (input.pinIsPressed(TouchPin.P1)) {
-        radio.sendValue("answer", 68)
-    }
-    
-    if (input.pinIsPressed(TouchPin.P2)) {
-        radio.sendValue("answer", 69)
+    if (canVote) {
+        if (input.buttonIsPressed(Button.A)) {
+            radio.sendValue("answer", 0)
+        }
+        
+        if (input.buttonIsPressed(Button.B)) {
+            radio.sendValue("answer", 1)
+        }
+        
+        if (input.pinIsPressed(TouchPin.P0)) {
+            radio.sendValue("answer", 2)
+        }
+        
+        if (input.pinIsPressed(TouchPin.P1)) {
+            radio.sendValue("answer", 3)
+        }
+        
+        if (input.pinIsPressed(TouchPin.P2)) {
+            radio.sendValue("answer", 4)
+        }
+        
     }
     
 })
 // pokud je hlas serverem přijat, server pošle potvrzení a klient zobrazí znak YES
+// klient také přijímá informaci zda je možno hlasovat, pokud není možno hlasovat tak zobrazí křížek
 radio.onReceivedValue(function on_received_value(name: string, value: number) {
+    let canVote: boolean;
     if (name == "confirm" && value == control.deviceSerialNumber()) {
         basic.showIcon(IconNames.Yes)
+    } else if (name == "voting") {
+        if (value == 0) {
+            canVote = false
+            basic.showIcon(IconNames.No, 1)
+        } else if (value == 1) {
+            canVote = true
+            basic.clearScreen()
+        }
+        
     }
     
 })
